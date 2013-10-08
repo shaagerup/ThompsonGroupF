43956bc4af863cb1fc5573f2c4f5d59adc24d6c9
Søren Haagerup <soren@artofweb.dk>
2013-10-08 14:41:18 +0200

    Changing data structure identifiers to economize space usage

--- BelkBrown.hs
+++ BelkBrown.hs
@@ -9,7 +9,7 @@ import Data.List
 import Data.Tuple
 
 data Generator = X0 | X1 | X0Inv | X1Inv deriving (Eq,Show)
-data Tree = Node Tree Tree | Leaf deriving (Eq,Show)
+data Tree = N Tree Tree | L deriving (Eq,Show)
 data Forest = Forest Int [Tree] deriving (Eq,Show)
 type FDiagram = (Forest,Forest) 
 -- first is domain, second is range. I.e. first is the bottom forest, second is the top forest! 
@@ -17,8 +17,8 @@ type FDiagram = (Forest,Forest)
 
 
 leafCount :: Tree -> Int
-leafCount Leaf = 1
-leafCount (Node t1 t2) = leafCount t1 + leafCount t2
+leafCount L = 1
+leafCount (N t1 t2) = leafCount t1 + leafCount t2
 
 fLeafCount :: [Tree] -> Int
 fLeafCount ts = sum $ map leafCount $ ts
@@ -28,36 +28,36 @@ op X0 (Forest p1 xs1, Forest p2 xs2) = (Forest p1 xs1', Forest p2' xs2')
 	where 
 		p2' = p2+1
 		(xs1',xs2')
-		 | p2' >= (length xs2) = (xs1 ++ [Leaf], xs2 ++ [Leaf])
+		 | p2' >= (length xs2) = (xs1 ++ [L], xs2 ++ [L])
 		 | otherwise = (xs1,xs2)
 
 op X1 (Forest p1 xs1, Forest p2 xs2) = (Forest p1 xs1', Forest p2 xs2')
 	where 
 		(ys1,ys2) = splitAt p2 xs2
 		(t1:t2:ys3) = case (length ys2) of
-			1 -> ys2++[Leaf]
+			1 -> ys2++[L]
 			_ -> ys2
-		xs2' = ys1 ++ [Node t1 t2] ++ ys3
+		xs2' = ys1 ++ [N t1 t2] ++ ys3
 		xs1'
-		 | fLeafCount xs2' > fLeafCount xs1 = xs1++[Leaf]
+		 | fLeafCount xs2' > fLeafCount xs1 = xs1++[L]
 		 | otherwise = xs1
 
 op X0Inv (Forest p1 xs1, Forest p2 xs2) = (Forest p1' xs1', Forest p2' xs2')
 	where
 		p1' = if p2>0 then p1 else p1+1
-		xs1' = if p2>0 then xs1 else [Leaf]++xs1
+		xs1' = if p2>0 then xs1 else [L]++xs1
 		p2' = if p2>0 then p2-1 else p2
-		xs2' = if p2>0 then xs2 else [Leaf]++xs2
+		xs2' = if p2>0 then xs2 else [L]++xs2
 
 op X1Inv (Forest p1 xs1, Forest p2 xs2) = (Forest p1 xs1', Forest p2 xs2')
 	where 
 		e = xs2!!p2
 		xs2e = case e of 
-			Leaf -> [Leaf, Leaf]
-			Node t1 t2 -> [t1,t2]
+			L -> [L, L]
+			N t1 t2 -> [t1,t2]
 		xs2' = take p2 xs2 ++ xs2e ++ drop (p2+1) xs2
 		xs1' = case e of 
-			Leaf -> snd $ modifyNthLeafPlural (fLeafCount $ take p2 xs2) xs1 (Node Leaf Leaf)
+			L -> snd $ modifyNthLeafPlural (fLeafCount $ take p2 xs2) xs1 (N L L)
 			_ -> xs1
 
 
@@ -71,10 +71,10 @@ op' X1Inv = trimTail . reduce . (op X1Inv)
 
 -- replaces nth leaf with l'
 modifyNthLeaf :: Int -> Tree -> Tree -> (Int,Tree)
-modifyNthLeaf n Leaf l'
+modifyNthLeaf n L l'
  | n == 0 = (n-1, l')
- | n /= 0 = (n-1, Leaf)
-modifyNthLeaf n (Node t1 t2) l' = (n'', Node t1' t2')
+ | n /= 0 = (n-1, L)
+modifyNthLeaf n (N t1 t2) l' = (n'', N t1' t2')
 	where 
 		(n',t1') = modifyNthLeaf n t1 l'
 		(n'', t2') = modifyNthLeaf n' t2 l'
@@ -87,9 +87,9 @@ modifyNthLeafPlural n (t:ts) l' = (n'', t':ts')
 		(n'',ts') = modifyNthLeafPlural n' ts l'
 
 carets :: Int -> Tree -> [Int] -> (Int,[Int])
-carets n (Leaf) cs = (n+1,cs)
-carets n (Node Leaf Leaf) cs = (n+2, n:cs)
-carets n (Node t1 t2) cs = (n'',cs'')
+carets n (L) cs = (n+1,cs)
+carets n (N L L) cs = (n+2, n:cs)
+carets n (N t1 t2) cs = (n'',cs'')
 	where 
 		(n',cs') = carets n t1 cs
 		(n'',cs'') = carets n' t2 cs'
@@ -108,11 +108,11 @@ opposingCarets :: FDiagram -> [Int]
 opposingCarets (Forest _ ts1, Forest _ ts2) = intersect (treelistCarets ts1) (treelistCarets ts2) 
 
 removeNthCaret :: Int -> Tree -> (Int,Tree)
-removeNthCaret n (Leaf) = (n-1,Leaf)
-removeNthCaret n t@(Node Leaf Leaf) 
- | n==0 = (n-2, Leaf)
+removeNthCaret n (L) = (n-1,L)
+removeNthCaret n t@(N L L) 
+ | n==0 = (n-2, L)
  | otherwise = (n-2, t)
-removeNthCaret n (Node t1 t2) = (n'',Node t1' t2')
+removeNthCaret n (N t1 t2) = (n'',N t1' t2')
 	where
 		(n',t1') = removeNthCaret n t1
 		(n'',t2') = removeNthCaret n' t2 
@@ -141,7 +141,7 @@ mirror :: FDiagram -> FDiagram
 mirror (Forest p1 ts1, Forest p2 ts2) = (Forest (length ts1 - p1 - 1) (reverse ts1),Forest (length ts2 - p2 - 1) (reverse ts2))
 
 trimHead :: FDiagram -> FDiagram
-trimHead d@(Forest p1 (Leaf:ts1), Forest p2 (Leaf:ts2))
+trimHead d@(Forest p1 (L:ts1), Forest p2 (L:ts2))
  | p1 > 0 && p2 > 0 = (Forest (p1-1) ts1, Forest (p2-1) ts2)
  | otherwise = d
 trimHead d = d
@@ -154,7 +154,7 @@ trimTail = mirror . trimHead . mirror
 --main = mapM_ putStrLn $ map (show . length . trivialReps) [1..]
 
 trivialDiagram :: FDiagram
-trivialDiagram = (Forest 0 [Leaf], Forest 0 [Leaf]) 
+trivialDiagram = (Forest 0 [L], Forest 0 [L]) 
 
 evaluate :: [Generator] -> FDiagram
 evaluate = foldr op' trivialDiagram
